6ded97aae584d6e6210a5d1b7444c84eb9f31cc6
Karolina Tchiling <kdt@my.yorku.ca>
2026-04-11 18:51:31 -0400

    Update resume + UI change to about me

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -37,10 +37,11 @@ export default function Home() {
   };
   
 return (
-<div className="scrollable min-h-screen flex flex-col justify-between items-center p-8 pb-10 sm:p-20 sm:pb-10 sm:pr-10 font-[family-name:var(--font-geist-sans)]">
+<div className="scrollable min-h-screen flex flex-col justify-between items-center p-8 pb-10 sm:p-14 sm:pb-10 sm:pr-10 font-[family-name:var(--font-geist-sans)]">
   
   <main className="flex-grow">
 
+      {/* HEADER */}
       <div className="flex flex-row gap-4 max-w-5xl w-full mx-auto font-[family-name:var(--font-geist-mono)]">
             <button
               onClick={() => handlePage("projects")}
@@ -57,27 +58,26 @@ return (
             </button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 max-w-5xl w-full mx-auto font-[family-name:var(--font-geist-mono)] mt-5">
-            {/* Column 1 */}
-            <div className="flex flex-col gap-6 text-center sm:text-left">
-              <div className="text-4xl font-bold tracking-tight leading-tight animate-fade-in">
-                <span className="opacity-80 block sm:inline">From mapping landscapes</span> <br className="hidden sm:block" />
-                <span className="opacity-100 font-extrabold text-[var(--accent-color)]">to building digital ones —</span>
-              </div>
-            </div>
-
-            {/* Column 2 */}
-            <div className="flex flex-col gap-6 text-center sm:text-left sm:justify-center">
-              <div className="text-2xl font-medium leading-snug animate-fade-in delay-200">
-                Hi, I'm <span className="font-bold text-[var(--accent-color)]">Karolina</span>, an ex-geology graduate turned computer science student, researcher and data scientist; passionate about ML, geo-spatial data, data-driven insights, and software solutions.
-              </div>
-            </div>
-      </div>
-
-
-
+      {/* CONTENT */}
       {activeSection === "projects" && (
-      <div id="projects">
+        <div id="projects">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 max-w-5xl w-full mx-auto font-[family-name:var(--font-geist-mono)] mt-5">
+                {/* Column 1 */}
+                <div className="flex flex-col gap-6 text-center sm:text-left">
+                  <div className="text-4xl font-bold tracking-tight leading-tight animate-fade-in">
+                    <span className="opacity-80 block sm:inline">From mapping landscapes</span> <br className="hidden sm:block" />
+                    <span className="opacity-100 font-extrabold text-[var(--accent-color)]">to building digital ones —</span>
+                  </div>
+                </div>
+
+                {/* Column 2 */}
+                <div className="flex flex-col gap-6 text-center sm:text-left sm:justify-center">
+                  <div className="text-2xl font-medium leading-snug animate-fade-in delay-200">
+                    Hi, I'm <span className="font-bold text-[var(--accent-color)]">Karolina</span>, an ex-geology graduate turned computer science student, researcher and data scientist; passionate about ML, geo-spatial data, data-driven insights, and software solutions.
+                  </div>
+                </div>
+          </div>
+
             <div id="accordion-section" className="gap-24 flex justify-center sm:justify-center w-full mt-8 animate-fade-in delay-400 animate-fade-in opacity-0">
               <button onClick={() => handleChange('panel1')} className="flex flex-col items-center group">
               <div className="animate-fade-in opacity-0">
@@ -117,17 +117,17 @@ return (
          <div id="about" className="max-w-5xl w-full animate-fade-in delay-400">
 
                     
-          <div className="flex flex-row items-center gap-12 mt-12">
+          <div className="flex flex-row items-center gap-8 mt-8">
 
-                  <div>
+                  <div className="basis-1/5">
                     <img src="/headshot.jpg" alt="Picture of Karolina" className="rounded-full" />
                   </div>
-                  <div className="text-right md:text-justify leading-relaxed text-lg font-thin font-[family-name:var(--font-geist-mono)]">
-                    My goal is to apply data science and computational thinking to improve the reliability and efficiency of large-scale systems. I’m particularly interested in building AI and automation solutions that support human decision-making and allow teams to focus more on innovation and creative problem solving. I’m especially interested in industries such as natural resources, utilities, infrastructure, and space.
+                  <div className="basis-5/6 text-right md:text-justify leading-relaxed text-lg font-thin font-[family-name:var(--font-geist-mono)]">
+                    I strive to apply data science and computational thinking to improve the reliability and efficiency of large-scale systems. I’m particularly interested in building AI and automation solutions that support human decision-making and allow teams to focus more on innovation and creative problem solving. I’m especially interested in industries such as natural resources, utilities, infrastructure, and space.
                   </div>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 max-w-5xl mt-12 w-full mx-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-5xl mt-3 w-full mx-auto">
 
                   <div>
                           <div className="flex flex-col">
@@ -135,29 +135,29 @@ return (
                                     Co-op and Internship Experience
                                   </div>
 
-                                  <div className="mt-4 grid grid-cols-[auto_1fr] gap-3 gap-x-2 text-base font-medium text-white">
+                                  <div className="mt-4 grid grid-cols-[auto_1fr] gap-3 gap-x-4 text-base font-medium text-white">
 
-                                    <div>Hydro One — </div>
+                                    <div>Hydro One Networks </div>
                                     <div>
-                                      <span> Data & Analytics </span>
-                                      <span className="font-thin italic">(16-month Co-op)</span>
+                                      <div> Data Analyst & Data Engineer  </div>
+                                      <div className="font-thin italic text-sm">(16-month Co-op)</div>
                                     </div>
 
-                                    <div>DARE! Lab — </div>
+                                    <div>DARE! Lab @ YorkU </div>
                                     <div>
-                                      <div> Data Science & Bibliometric Research</div>
-                                      <div className="font-thin italic">(4-month Co-op; 8-month part-time Research Assistant)</div>
+                                      <div> Data Science & Software Developer</div>
+                                      <div className="font-thin italic text-sm">(4-month Co-op; 8-month part-time Research Assistant)</div>
                                     </div>
 
-                                    <div>GNSS Lab — </div>
+                                    <div>GNSS Lab @ YorkU </div>
                                     <div>
-                                      <div> Machine Learning & Geospatial Research</div>
-                                      <div className="font-thin italic">(4-month Internship; 8-month part-time Research Assistant)</div>
+                                      <div> Geospatial Machine Learning Developer</div>
+                                      <div className="font-thin italic text-sm">(4-month Internship; 8-month part-time Research Assistant)</div>
                                     </div>
 
                                   </div>
 
-                                  <p className="mt-4 text-base text-gray-300">
+                                  <p className="mt-4 text-sm text-gray-300">
                                     The most rewarding part of my co-op experiences has been seeing my work translate into tangible impact. In research, this has meant contributing to published work that advances the scientific community. In industry, it has meant building automated pipelines that replace repetitive tasks—work computers are better suited for—allowing people to focus on creative thinking, problem solving, and innovation.
                                   </p>
                             </div> 
@@ -167,15 +167,16 @@ return (
                         <div className="text-xl font-bold">BSc Computer Science @ York University</div>
                         <div className="text-gray-30 italic">4th Year | 20-Month Co-op Program</div>
 
-                        <div className="mt-2 text-base text-gray-300">
+                        <div className="mt-2 text-sm text-gray-300">
                           What I love most about computer science is the way it teaches you to think in algorithms. I enjoy decomposing complex problems into structured, logical steps and translating those solutions into processes a computer can execute.
                         </div>
                       </div>
 
-                      <div className="w-full aspect-video my-6">
+
+                      <div className="w-full h-[270px] overflow-hidden flex items-center justify-center rounded-xl mt-5">
                         <iframe
                           src="https://drive.google.com/file/d/1bC7u19YoPts-HNPvWI20h7S1SA8T29Sd/preview"
-                          className="w-full h-full rounded-xl"
+                          className="w-full h-[298px]"
                           allow="autoplay"
                         />
                       </div>
